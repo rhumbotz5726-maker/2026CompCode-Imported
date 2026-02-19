d827d90ab4662dc354d7800a07b0cb592aaf6803
5726 RHUMBOTZ <rhumbotz5726@gmail.com>
2026-02-19 16:29:31 -0600

    shooter line up pid

--- src/main/java/frc/robot/commands/ShooterLineUpCmd.java
+++ src/main/java/frc/robot/commands/ShooterLineUpCmd.java
@@ -1,10 +1,19 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ShooterSubsystem;
 
 public class ShooterLineUpCmd extends Command{
+        ShooterSubsystem sub;
+        public ShooterLineUpCmd(ShooterSubsystem sub) {
+                this.sub=sub;
+                //this.pid
+        }
 
-        public ShooterLineUpCmd() {
-                
+
+        @Override
+        public void initialize() {
+        // TODO Auto-generated method stub
+        super.initialize();
         }
 }
